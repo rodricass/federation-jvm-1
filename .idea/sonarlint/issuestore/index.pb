68a422e862b5d94e195ea29ebfa3c97e67d97f5e
Rodrigo <rodrigo.ncassanelli@gmail.com>
2020-10-16 16:13:04 -0300

    Remove federated tracing

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+¤
+tgraphql-java-support/src/test/java/com/apollographql/federation/graphqljava/FederatedTracingInstrumentationTest.java,3/8/384a7a1609674027ca76414667a8a92a8dda984d
+€
+Pspring-example/src/main/java/com/apollographql/federation/springexample/App.java,a/b/abd933610c1bc065d57a6219e6a7644e8231158d
+¨
+xgraphql-java-support/src/main/java/com/apollographql/federation/graphqljava/tracing/FederatedTracingInstrumentation.java,c/b/cb6b9288491c48a8fc76c203063a7a64bf26c3f9
+”
+dspring-example/src/main/java/com/apollographql/federation/springexample/InventorySchemaProvider.java,4/f/4fe6c6ff5bb56d79e861cbe589928f0a221180b8
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
--- spring-example/src/main/java/com/apollographql/federation/springexample/App.java
+++ spring-example/src/main/java/com/apollographql/federation/springexample/App.java
@@ -1,6 +1,5 @@
 package com.apollographql.federation.springexample;
 
-import com.apollographql.federation.graphqljava.tracing.FederatedTracingInstrumentation;
 import graphql.execution.instrumentation.Instrumentation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,10 +9,5 @@ import org.springframework.context.annotation.Bean;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Bean
-    public Instrumentation addFederatedTracing() {
-        return new FederatedTracingInstrumentation(new FederatedTracingInstrumentation.Options(true));
     }
 }
